23c915e9de14b6d072de2fbf9d4ac18e6c1d92d0
Opilane TTHK <opilane@tthk.ee>
2020-01-14 09:23:36 +0200

    fgqwefg

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ document.querySelector(".btn").addEventListener("click", BMIValueCalcutation);
 function BMIValueCalcutation(){
     var height = document.getElementById("height").value;
     var weight = document.getElementById("weight").value;
-    var bmivalue = weight  / (height/100 * height/100);
+    var bmivalue = weight  / (height/100 * height/100);  
 
-    document.getElementById("Value").innerHTML = "BMI Value: " + bmivalue;  
+    if(bmivalue < 18.5){
+        document.getElementById("Value").innerHTML ="BMI Value: " + bmivalue +" : Underweight";
+    };
+    if(bmivalue => 18.5 || bmivalue <= 24.9){
+        document.getElementById("Value").innerHTML ="BMI Value: " + bmivalue +" : Normal weight";
+    };
+    if(bmivalue => 25 || bmivalue <= 29.9){
+        document.getElementById("Value").innerHTML ="BMI Value: " + bmivalue +" : Overweight";
+    };
+    if(bmivalue > 30){
+        document.getElementById("Value").innerHTML ="BMI Value: " + bmivalue +" : Underweight";
+    };
 
     event.preventDefault();
 }
